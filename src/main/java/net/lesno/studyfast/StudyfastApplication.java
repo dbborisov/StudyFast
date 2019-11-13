838870242127d99c2d3e8a52506f02814b1501fb
dbborisov <dobromiir.borisov24@gmail.com>
2019-11-13 12:36:28 +0200

    Initial commit

---
+++ src/main/java/net/lesno/studyfast/StudyfastApplication.java
@@ -0,0 +1,13 @@
+package net.lesno.studyfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyfastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyfastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/lesno/studyfast/StudyfastApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lesno.studyfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyfastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
